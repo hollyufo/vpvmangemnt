b792f4e9ea8699bd395547ce30ed0bfd8f30b79c
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-02-22 12:04:41 +0100

    department crud done

---
+++ removedep.php
@@ -0,0 +1,12 @@
+<?php 
+    
+    include('config/db.php');
+    if(!$_SESSION['login']){
+      header("location: index.php");
+      die;
+   }
+   else{
+    $dep_id = $_GET['id'];
+    mysqli_query($connection, "DELETE FROM dep WHERE depid=$dep_id");
+    echo "<script>window.location.href = './departement.php';</script>";
+   }
